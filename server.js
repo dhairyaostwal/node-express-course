edfbad82e625ae136fb05fa456386bed634ce71e
Dhairya <dhairya.ostwal@gmail.com>
2021-03-03 18:29:14 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData = [
     { name: 'Mark' },
@@ -16,6 +19,7 @@ app.get('/users', function (req, res) {
 })
 
 // dynamic GET route
+// colons are used as variables that be viewed in the params
 app.get('/user/:id', function (req, res) {
     console.log(req.params.id)
     res.json({
@@ -23,6 +27,29 @@ app.get('/user/:id', function (req, res) {
         message: 'got one user',
         users: req.params.id
     })
+})
+
+// POST route
+app.post('/login', function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if(username===mockPassword && password===mockPassword){
+        req.json({
+            success:true,
+            message:'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    }
+    else{
+        res.json({
+            success:false,
+            message:'password and username do not match'
+        })
+    }
 })
 
 
